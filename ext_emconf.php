f211cfe7a84dc3ac44dd99818c2a1dddd2e3667f
Stig Nørgaard Færch <stigfaerch@gmail.com>
2019-06-25 15:43:51 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,9 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '2.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.4-8.99.99',
-			'php' => '5.3.0-0.0.0',
-			'news' => '3.0.0',
+
 		),
 		'conflicts' => array(
 		),
